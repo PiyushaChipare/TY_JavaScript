cf455581fe7380182dd035e271d6de97c5f9dbb0
Piyusha <pushachip@gmail.com>
2023-06-14 14:33:38 +0530

    c2

---
+++ LoopStatements/ForIn.js
@@ -0,0 +1,4 @@
+var a=[11,21,31,41,51,61,71,81,91];
+for (const index in a) {
+   console.log(index+ " "+ a[index]);
+}
---
+++ LoopStatements/Forof.js
@@ -0,0 +1,16 @@
+var v=['a','b','c']
+for (const iterator of v) 
+{
+ console.log(iterator);   
+}
+v.forEach(element => 
+    {
+    console.log(element+element);
+    })
+
+    v.forEach(num =>
+        {
+        console.log(num)
+        console.log(num)
+        }
+    )
