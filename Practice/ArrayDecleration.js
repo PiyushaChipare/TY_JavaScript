b0ab599003c96c7c6087029756d1c2d5c1fc3a5e
Piyusha <pushachip@gmail.com>
2023-06-15 10:15:13 +0530

    c5

---
+++ Practice/ArrayDecleration.js
@@ -0,0 +1,4 @@
+//let a=new Array(3,'hi','hello','l',true)
+//a=[3,2,8,4,6,'hi']
+console.log(a);
+const a=[3,2,8,4,6,'hi']
---
+++ StringPrograms/MaamProgram.js
@@ -0,0 +1,20 @@
+let s='Testyantra'
+for (let i = 0; i < s.length; i++) 
+{
+    let count=0;
+    for (let j = 0; j < s.length; j++) 
+    {
+       if (s.charAt(i)==s.charAt(j)) 
+       {            
+            if(j<i)
+            {
+                break;
+            }
+            count++;
+       } 
+    }
+    if (count>=1)
+        {
+            console.log(s.charAt(i)+" "+count);
+        }
+}
